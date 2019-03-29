2e58a622cfe8dc85cd9808f79096780c6414c26b
Damian Walczak <losdamianos@gmail.com>
2019-03-29 22:01:36 +0000

    first commit

---
+++ TalesFromUnexpected/ListExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TalesFromUnexpected
+{
+    public static class ListExtensions
+    {
+        public static T PickRandom<T>(this List<T> enumerable)
+        {
+            int index = new Random().Next(0, enumerable.Count());
+            return enumerable[index];
+        }
+    }
+}
